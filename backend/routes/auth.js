e435e27b33d08720511465a2fa4d9240f92ae3bd
Luis Chilo <gfxargentina@gmail.com>
2021-06-04 12:00:58 -0300

    add express-validator

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -6,15 +6,31 @@
 const { Router } = require('express');
 const router = Router();
 
+
 const { crearUsuario, login, revalidarToken } = require('../controllers/auth');
+const { validarCampos } = require('../middlewares/validar-campos');
+const { check } = require('express-validator');
 
 
 
 //ruta endpoint nuevo usuario
-router.post('/new', crearUsuario );
+router.post('/new',
+              [//middlewares
+                check('nombre', 'El nombre es obligatorio').not().isEmpty(),
+                check('email', 'El email es obligatorio').isEmail(),
+                check('password', 'El password debe de tener 8 caracteres como minimo').isLength({ min: 8}),
+                validarCampos
+
+              ], crearUsuario );
 
 //ruta endpoint Login
-router.post('/', login );
+router.post('/', 
+            [
+                check('email', 'El email es obligatorio').isEmail(),
+                check('password', 'El password es obligatorio').not().isEmpty(),
+                validarCampos
+                
+            ], login );
 
 //ruta endpoint revalidar token
 router.get('/renew', revalidarToken );

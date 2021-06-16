dae48657e57c2ea3daa8f5b3e0b4231dfcec140f
Luis Chilo <gfxargentina@gmail.com>
2021-06-16 12:25:46 -0300

    http authentication

--- frontend/frontend/src/actions/auth.js
+++ frontend/frontend/src/actions/auth.js
@@ -1,8 +1,31 @@
+import { fetchSinToken } from "../helpers/fetch"
+import { types } from "../types/types";
+
 
 
 export const startLogin = ( email, password ) => {
-    return async() => {
+    return async( dispatch ) => {
         //verificar si llegan el email y pass
         //console.log(email, password)
+
+        const resp = await fetchSinToken( 'auth', { email, password }, 'POST' );
+        const body = await resp.json();
+        //console.log(body)
+
+        if( body.ok ) {
+            localStorage.setItem('token', body.token );
+            localStorage.setItem('token-init-date', new Date().getTime() );
+
+            dispatch ( login({
+                uid: body.uid,
+                name: body.name
+            }))
+        }
     }
 }
+
+const login = ( user ) => ({
+    type: types.authLogin,
+    payload: user
+})
+
--- frontend/frontend/src/reducers/authReducer.js
+++ frontend/frontend/src/reducers/authReducer.js
@@ -1,3 +1,5 @@
+import { types } from "../types/types";
+
 
 const initialState = { 
     checking: true
@@ -5,7 +7,13 @@ const initialState = {
 
 export const authReducer = ( state = initialState, action ) => {
     switch ( action.type ) {
-           
+        
+        case types.authLogin: 
+            return {
+                ...state,
+                checking: false,
+                ...action.payload
+            }
     
         default:
             return state;

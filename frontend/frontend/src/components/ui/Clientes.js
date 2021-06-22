0f68f521dd343ab6f13d1d0ed8e46a02ad760237
Luis Chilo <gfxargentina@gmail.com>
2021-06-22 18:18:09 -0300

    delete image code

--- frontend/frontend/src/components/ui/Clientes.js
+++ frontend/frontend/src/components/ui/Clientes.js
@@ -93,7 +93,7 @@ export const Clientes = () => {
                     <td className="px-6 py-4 whitespace-nowrap">
                       <div className="flex items-center">
                         <div className="flex-shrink-0 h-10 w-10">
-                            <img className="h-10 w-10 rounded-full"  alt="" />  
+                            {/* <img className="h-10 w-10 rounded-full"  alt="" />   */}
                         </div>
                         <div className="ml-4">
                           <div className="text-sm font-medium text-gray-900">{person.nombreApellido}</div>

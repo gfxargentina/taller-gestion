8e43931a968519624f58df635d7ba952db074fe1
Luis Chilo <gfxargentina@gmail.com>
2021-08-14 12:57:13 -0300

    style aparato details

--- frontend/frontend/src/components/taller/AparatoDetalle.js
+++ frontend/frontend/src/components/taller/AparatoDetalle.js
@@ -20,38 +20,46 @@ export const AparatoDetalle = () => {
 	return (
 		<>
       <div className="bg-blue-400 min-h-screen flex items-center justify-center">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 " >
-          <div className="flex flex-col col-auto">
-            <label className="col-auto">Fecha de Entrada</label>
-            <input type="text" className="rounded-md p-2" value={ dayjs(aparato.fechaEntrada).format("DD/MM/YYYY") } />
+        <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-2" >
+          <div className="flex flex-col">
+            <label className="text-center">Fecha de Entrada</label>
+            {/* <input type="text" className="rounded-md p-2" value={ dayjs(aparato.fechaEntrada).format("DD/MM/YYYY") } /> */}
+            <div className="flex-none bg-white rounded-md p-2 text-center">
+            { dayjs(aparato.fechaEntrada).format("DD/MM/YYYY") }
+            </div>
+          </div>
+          <div className="flex flex-col ">
+            <label className="text-center">Aparato</label>
+            {/* <input type="text" className="rounded-md p-2" value={ aparato.aparato } /> */}
+            <div className="bg-white rounded-md p-2">
+            { aparato.aparato }
+            </div>
           </div>
           <div className="flex flex-col">
-            <label className="">Aparato</label>
-            <input type="text" className="rounded-md p-2" value={ aparato.aparato } />
+            <label className="text-center">Estado</label>
+            <div className="bg-white rounded-md p-2 text-center">
+                En Observacion            
+            </div>
           </div>
           <div className="flex flex-col">
-            <label className="">Retirado</label>
-            <input type="text" className="rounded-md p-2" value="" />
-          </div>
-          <div className="flex flex-col">
-            <label className="col-auto">Precio</label>
+            <label className="col-auto">Fecha de Retirado</label>
             <input type="text" className="rounded-md p-2" value="$25.000" />
           </div>
           <div className="flex flex-col md:col-span-2">
             <label className="col-auto">Falla</label>
-            <input type="text" className="rounded-md p-2" value={ aparato.falla } />
+            <div className="bg-white rounded-md p-2 h-24 max-w-prose"> {aparato.falla} </div>
           </div>
           <div className="flex flex-col md:col-span-2">
-            <label className="col-auto">Observaciones</label>
-            <input type="text" className="rounded-md p-2" value={ aparato.observaciones } />
+            <label className="">Observaciones</label>
+            <div className="bg-white rounded-md p-2 h-24 max-w-prose"> {aparato.observaciones} </div>
           </div>
-          <div className="flex flex-col md:col-span-3">
-            <label className="col-auto">Presupuesto</label>
-            <input type="text" className="rounded-md p-2" value={ aparato.presupuesto } />
+          <div className="flex flex-col md:col-span-2">
+            <label className="col-auto">Presupuesto</label>            
+            <div className="bg-white rounded-md p-2 h-24 max-w-prose" > {aparato.presupuesto}  </div>
           </div>
-          <div className="flex flex-col">
-            <label className="col-auto">Precio</label>
-            <input type="text" className="rounded-md p-2" value="$15.000" />
+          <div className="flex flex-col md:col-span-1">
+            <label className="col-auto">Precio</label>            
+            <div className="bg-white rounded-md p-2 w-24 " >$16.000</div>
           </div>
         </div>
       </div>

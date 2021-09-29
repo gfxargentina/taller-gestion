2fc48873bdc817fd8b4fd19b18f34038b6b44967
Luis Chilo <gfxargentina@gmail.com>
2021-09-29 18:16:13 -0300

    Feature searchbar

--- frontend/frontend/src/components/ui/Clientes.js
+++ frontend/frontend/src/components/ui/Clientes.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import dayjs from "dayjs";
 import { useDispatch, useSelector} from 'react-redux';
 import { startGetClients, setActiveClient, deleteClient } from '../../actions/clients';
@@ -8,6 +8,15 @@ import { ClientModal } from '../taller/ClientModal';
 import { startGetAparatos } from '../../actions/aparatos';
 import Swal from 'sweetalert2';
 
+
+ function searchTerm(term)  {   
+   return function(x){
+     //console.log(typeof x);        
+     //return x.nombreApellido.toLowerCase().includes(term) || !term;     
+     return x.dni.toString().includes(term);
+     
+   }
+ }
 
 
 
@@ -68,17 +77,33 @@ export const Clientes = () => {
      
   }
 
+    //search bar
+    
+    const [data, setData] = useState([]);
+    const [term, setTerm] = useState(''); 
+    
+    
 
+    useEffect(() => {
+      setData(clientes);
+    }, [clientes])
+
+    
 
     return (
-    <> 
-       
+    <>        
           {
-            ( clientes.length > 0 ) ? (
+            ( data.length > 0 ) ? (
               <div>
               <div className="flex flex-col container mx-auto">              
               <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                 <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                <h1>Buscar Cliente por DNI o Apellido y Nombre</h1>
+                        <input type="text" 
+                                name="term"
+                                placeholder="DNI o Apellido y nombre"
+                                onChange={ e => setTerm(e.target.value)}
+                        />
                   <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
                   <Link to="/nuevo-cliente">
                   <button class="h-10 px-5 mt-5 mb-5 text-indigo-700 transition-colors duration-150 border border-indigo-500 rounded-lg focus:shadow-outline 
@@ -122,7 +147,7 @@ export const Clientes = () => {
                           <tbody className="bg-white  divide-y divide-gray-300">
                         {/* el ? se usa para que cuando llame a la api no de undefined,
                          porque el componente se carga primero y los datos todavia no estan listos */}
-                        {clientes?.map((person) => (
+                        {data?.filter(searchTerm(term)).map((person) => (
                           <tr key={person.id}>
                             <td className="px-6 py-4 whitespace-nowrap">
                               <div className="flex items-center">                                

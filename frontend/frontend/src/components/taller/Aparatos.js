b1c85ec681d7c909e05cbb412b5795b4ea9078e5
Luis Chilo <gfxargentina@gmail.com>
2021-09-29 20:28:11 -0300

    fix button position

--- frontend/frontend/src/components/taller/Aparatos.js
+++ frontend/frontend/src/components/taller/Aparatos.js
@@ -4,7 +4,7 @@ import { GetAparatos } from '../ui/GetAparatos'
 
 export const Aparatos = () => {
     return (
-        <div>
+        <div className="flex flex-col container mx-auto">
             <Link to="/nuevo-aparato">
                   <button class="h-10 px-5 mt-5 mb-5 text-indigo-700 transition-colors duration-150 border border-indigo-500 rounded-lg focus:shadow-outline 
                               hover:bg-indigo-500 hover:text-indigo-100">Nuevo Aparato</button>

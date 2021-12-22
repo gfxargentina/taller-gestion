9a040e87c3af81bdb0a7303b2a182dbbbec4bbc0
Luis Chilo <gfxargentina@gmail.com>
2021-12-22 00:25:57 -0300

    add typo styles prices

--- frontend/frontend/src/components/ui/Aparatos.jsx
+++ frontend/frontend/src/components/ui/Aparatos.jsx
@@ -145,7 +145,7 @@ export const GetAllAparatos = () => {
                           </td>
                           <td className="px-3 py-3 whitespace-nowrap text-center text-lg font-medium text-white">
                             {" "}
-                            {aparato.precio}{" "}
+                            ${aparato.precio}{" "}
                           </td>
                           <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
                             <Link to="/detalle-aparato2">

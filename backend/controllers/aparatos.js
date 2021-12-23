d8e9adcaa9b1f2e3026340c65933005d90668272
Luis Chilo <gfxargentina@gmail.com>
2021-12-23 12:09:57 -0300

    sort aparatos result

--- backend/controllers/aparatos.js
+++ backend/controllers/aparatos.js
@@ -1,166 +1,151 @@
-const { response } = require('express');
-const Aparato = require('../models/Aparato');
-const Cliente = require('../models/Cliente');
-
-
-
+const { response } = require("express");
+const Aparato = require("../models/Aparato");
+const Cliente = require("../models/Cliente");
 
 //ingresar nuevo aparato
-const nuevoAparato = async( req, res = response ) => {
-    //console.log(req.body)
-    //console.log(res)
+const nuevoAparato = async (req, res = response) => {
+  //console.log(req.body)
+  //console.log(res)
 
-    const aparato = new Aparato(req.body);
-        
+  const aparato = new Aparato(req.body);
 
-    try {
-        
-         //trae el id del cliente y lo guarda en la coleccion aparato 
-         aparato.cliente = req.params.id;
+  try {
+    //trae el id del cliente y lo guarda en la coleccion aparato
+    aparato.cliente = req.params.id;
 
-         //trae el uid del usuario y lo guarda en la coleccion aparato
-         aparato.user = req.uid;
+    //trae el uid del usuario y lo guarda en la coleccion aparato
+    aparato.user = req.uid;
 
-         //guarda el aparato
-         const aparatoGuardado = await aparato.save();
-          
-          //actualiza el cliente con el nuevo aparato ingresado
-         const id = req.params.id;
-         await Cliente.findByIdAndUpdate(id, { $push: { aparatos: aparatoGuardado._id } } );
-               
-         
+    //guarda el aparato
+    const aparatoGuardado = await aparato.save();
 
-          res.status(201).json({
-            ok: true,
-            msg: 'Aparato guardado',
-            aparato: aparatoGuardado,
-            
-        })
+    //actualiza el cliente con el nuevo aparato ingresado
+    const id = req.params.id;
+    await Cliente.findByIdAndUpdate(id, {
+      $push: { aparatos: aparatoGuardado._id },
+    });
 
-        
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({ 
-            ok: false,
-            msg: 'Hable con el administrador'
-        })
-    }
-}
+    res.status(201).json({
+      ok: true,
+      msg: "Aparato guardado",
+      aparato: aparatoGuardado,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Hable con el administrador",
+    });
+  }
+};
 
 //obtener todos los aparatos
-const getAparatos = async( req , res = response ) => {
+const getAparatos = async (req, res = response) => {
+  const aparatos = await Aparato.find().sort([["fechaSalida", -1]]);
 
-     const aparatos = await Aparato.find();
-
-    
-
-    res.status(200).json({
-        ok: true,
-        aparatos
-    })
-}
+  res.status(200).json({
+    ok: true,
+    aparatos,
+  });
+};
 
 //Actualizar Aparato
-const actualizarAparato = async( req, res = response ) => {
+const actualizarAparato = async (req, res = response) => {
+  //id del aparato a actualizar
+  const aparatoId = req.params.id;
 
-    //id del aparato a actualizar
-    const aparatoId = req.params.id;
+  //trae el uid del usuario
+  const uid = req.uid;
 
-    //trae el uid del usuario
-    const uid = req.uid;
+  try {
+    //busca el aparato en la db por id
+    const aparato = await Aparato.findById(aparatoId);
 
-    try {
-        //busca el aparato en la db por id
-        const aparato = await Aparato.findById( aparatoId );
-
-        if( !aparato ) {
-            return res.status(404).json({
-                ok: false,
-                msg: 'No existe ningun aparato con ese id'
-            })
-        }
-        //verificar que sea el mismo usuario el que quiere realizar los cambios
-        if ( aparato.user.toString() !== uid ) {
-            return res.status(401).json({
-                ok: false,
-                msg: 'No esta autorizado a editar este cliente'
-            });
-        }
-
-        //datos actualizados
-        const aparatoEditado = { 
-            ...req.body,
-            user: uid
-        }
-
-        //busca el aparato para actualizar, findByIdAndUpdate recibe el id, los nuevos datos, y
-        //devuelve el nuevo aparato actualizado con new: true en la response
-        const aparatoActualizado = await Aparato.findByIdAndUpdate( aparatoId, aparatoEditado, { new: true } );
-        res.json({
-            ok: true,
-            aparatoActualizado
-        })
-
-
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            ok: false,
-            msg: 'Hable con el administrador'
-        });
+    if (!aparato) {
+      return res.status(404).json({
+        ok: false,
+        msg: "No existe ningun aparato con ese id",
+      });
     }
-}
+    //verificar que sea el mismo usuario el que quiere realizar los cambios
+    if (aparato.user.toString() !== uid) {
+      return res.status(401).json({
+        ok: false,
+        msg: "No esta autorizado a editar este cliente",
+      });
+    }
+
+    //datos actualizados
+    const aparatoEditado = {
+      ...req.body,
+      user: uid,
+    };
+
+    //busca el aparato para actualizar, findByIdAndUpdate recibe el id, los nuevos datos, y
+    //devuelve el nuevo aparato actualizado con new: true en la response
+    const aparatoActualizado = await Aparato.findByIdAndUpdate(
+      aparatoId,
+      aparatoEditado,
+      { new: true }
+    );
+    res.json({
+      ok: true,
+      aparatoActualizado,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Hable con el administrador",
+    });
+  }
+};
 
 // Borrar aparato
-const eliminarAparato = async( req, res = response ) => {
+const eliminarAparato = async (req, res = response) => {
+  //id del aparato a eliminar
+  const aparatoId = req.params.id;
 
-    //id del aparato a eliminar
-    const aparatoId = req.params.id;
+  //trae el uid del usuario
+  const uid = req.uid;
 
-    //trae el uid del usuario
-    const uid = req.uid;
+  try {
+    //busca el aparato en la db por id
+    const aparato = await Aparato.findById(aparatoId);
 
-    try {
-        //busca el aparato en la db por id
-        const aparato = await Aparato.findById( aparatoId );
-
-        if( !aparato ) {
-            return res.status(404).json({
-                ok: false,
-                msg: 'No existe ningun aparato con ese id'
-            })
-        }
-        //verificar que sea el mismo usuario el que quiere realizar los cambios
-        if ( aparato.user.toString() !== uid ) {
-            return res.status(401).json({
-                ok: false,
-                msg: 'No esta autorizado a eliminar este aparato'
-            });
-        }
-
-        //elimina el aparato
-        await Aparato.findByIdAndDelete( aparatoId );
-
-        res.json({
-            ok: true,
-            msg: 'Aparato eliminado'
-        })
-
-
-    } catch (error) {
-        console.log(error);
-        res.status(500).json({
-            ok: false,
-            msg: 'Hable con el administrador'
-        });
+    if (!aparato) {
+      return res.status(404).json({
+        ok: false,
+        msg: "No existe ningun aparato con ese id",
+      });
     }
-}
+    //verificar que sea el mismo usuario el que quiere realizar los cambios
+    if (aparato.user.toString() !== uid) {
+      return res.status(401).json({
+        ok: false,
+        msg: "No esta autorizado a eliminar este aparato",
+      });
+    }
 
+    //elimina el aparato
+    await Aparato.findByIdAndDelete(aparatoId);
 
+    res.json({
+      ok: true,
+      msg: "Aparato eliminado",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      msg: "Hable con el administrador",
+    });
+  }
+};
 
 module.exports = {
-    nuevoAparato,
-    getAparatos,
-    actualizarAparato,
-    eliminarAparato
-}
+  nuevoAparato,
+  getAparatos,
+  actualizarAparato,
+  eliminarAparato,
+};

f9e569a2a5e54530b33e088ee4f9c3a49fbad753
Luis Chilo <gfxargentina@gmail.com>
2022-02-26 10:57:45 -0300

    fix typo

--- frontend/frontend/src/components/ui/GetAparatos.js
+++ frontend/frontend/src/components/ui/GetAparatos.js
@@ -150,8 +150,7 @@ export const GetAparatos = () => {
                               : ""}
                           </td>
                           <td className="px-3 py-3 whitespace-nowrap text-center text-lg font-medium text-white">
-                            {" "}
-                            {aparato.precio}{" "}
+                            ${aparato.precio}
                           </td>
                           <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
                             <Link to="/detalle-aparato">

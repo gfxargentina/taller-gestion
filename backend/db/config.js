8799bb2708dec2f9c0f6cb36ef00e8421b0a4ad7
Luis Chilo <gfxargentina@gmail.com>
2022-02-27 13:57:11 -0300

    change backend connection - add new client option when no client in db

--- backend/db/config.js
+++ backend/db/config.js
@@ -1,22 +1,28 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const dbConection = async() => {
+const dbConection = async () => {
+  const url = "mongodb://127.0.0.1/taller";
+  try {
+    //Habilitar esto para conectar con mongodb atlas
+    // await mongoose.connect( process.env.DB, {
+    //     useNewUrlParser: true,
+    //     useUnifiedTopology: true,
+    //     useCreateIndex: true
+    // });
 
-    try {
-        await mongoose.connect( process.env.DB, {
-            useNewUrlParser: true, 
-            useUnifiedTopology: true,
-            useCreateIndex: true
-        });
-        
-        console.log('DB conectada')
-        
-    } catch (error) {
-        console.log(error);
-        throw new Error('No se puede conectar a la DB');
-    }
-}
+    await mongoose.connect(url, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+
+    console.log("DB conectada");
+  } catch (error) {
+    console.log(error);
+    throw new Error("No se puede conectar a la DB");
+  }
+};
 
 module.exports = {
-    dbConection
-}
+  dbConection,
+};
--- frontend/frontend/src/components/ui/NoHayClientes.js
+++ frontend/frontend/src/components/ui/NoHayClientes.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export const NoHayClientes = () => {
-    return (
-        <div>
-            <h1>No hay clientes en la base de datos, agregue uno</h1>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>No hay clientes en la base de datos, agregue uno</h1>
+      <Link to="/nuevo-cliente">
+        <button
+          class="h-10 px-5 mt-5 mb-5 text-indigo-700 transition-colors duration-150 border border-indigo-500 rounded-lg focus:shadow-outline 
+                              hover:bg-indigo-500 hover:text-indigo-100"
+        >
+          Nuevo Cliente
+        </button>
+      </Link>
+    </div>
+  );
+};
